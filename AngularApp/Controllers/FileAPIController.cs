ade271e52d350e9529fa63f322e46a70976f3ad4
Vaibhav Vyas <vaibhavvyas98@outlook.com>
2020-06-24 15:51:05 +0530

    Add project files.

---
+++ AngularApp/Controllers/FileAPIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Web;
+
+namespace AngularApp.Controllers
+{
+    public class FileAPIController : ApiController
+    {
+        [Route("api/FileAPI/UploadFiles")]
+        [HttpPost]
+        public HttpResponseMessage UploadFiles()
+        {
+            //Create the Directory.
+            string path = HttpContext.Current.Server.MapPath("~/Uploads/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            //Save the Files.
+            foreach (string key in HttpContext.Current.Request.Files)
+            {
+                HttpPostedFile postedFile = HttpContext.Current.Request.Files[key];
+                postedFile.SaveAs(path + postedFile.FileName);
+            }
+
+            //Send OK Response to Client.
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+    }
+}
